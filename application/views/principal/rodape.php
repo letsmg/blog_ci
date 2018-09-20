521628854313961c61d5e5a92ac1920be6485ba2
letsmg <luizedmg@gmail.com>
2018-09-20 10:21:55 -0300

    correcao

--- application/views/principal/rodape.php
+++ application/views/principal/rodape.php
@@ -4,7 +4,7 @@
         <footer class="text-center mt-auto bg-secondary text-white py-3">
             Blog CI - Template Básico para Desenvolvedores 2018
             <br>
-            Dinsponível por <a href="http://www.sitesluiz.esy.es/home/antigo">Luiz</a>
+            Disponibilizado por <a class="text-white" href="http://www.sitesluiz.esy.es/home/antigo">Luiz</a>
             <br>
             Tecnologias usadas: PHP, Jquery, Boostrap 4, Laravel Mix, MariaDb
         </footer>
