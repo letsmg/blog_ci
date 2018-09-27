0abc3623eb271ee4bc5ab9dc7833119a5df10e12
letsmg <luizedmg@gmail.com>
2018-09-27 13:08:37 -0300

    melhorias

--- application/views/principal/rodape.php
+++ application/views/principal/rodape.php
@@ -1,12 +1,12 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
 ?>            
-        <footer class="text-center mt-auto bg-tema text-white">
+        <footer class="text-center mt-auto bg-tema text-white py-3">
             Blog CI - Template Básico para Desenvolvedores 2018
             <br>
             Disponibilizado por <a class='text-white' href="http://www.sitesluiz.esy.es/home/antigo" target="_blank">Luiz</a>
             <br>
-            Tecnologias usadas: PHP, Jquery, Boostrap 4, Laravel Mix, MariaDb
+            Tecnologias usadas: PHP + Codeigniter, JQuery, Boostrap 4, Laravel Mix, MariaDb
         </footer>
         
     </body>
